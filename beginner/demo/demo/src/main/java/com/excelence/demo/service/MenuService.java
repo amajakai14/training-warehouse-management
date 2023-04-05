f03580e59275f1ca3faf30ab614e1df5d68bc502
means <amajakai14@gmail.com>
2023-04-05 10:18:24 +0900

    Return Menu Model

--- beginner/demo/demo/src/main/java/com/excelence/demo/service/MenuService.java
+++ beginner/demo/demo/src/main/java/com/excelence/demo/service/MenuService.java
@@ -1,6 +1,6 @@
 package com.excelence.demo.service;
 
-import com.excelence.demo.model.ExampleOrder;
+import com.excelence.demo.model.ExampleMenu;
 import com.excelence.demo.repository.MenuRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +10,8 @@ import java.util.List;
 public class MenuService {
     private final MenuRepository repository;
 
-    public List<ExampleOrder> getAllOrder() {
-        //TODO call method from repository and fix the return value
-        return null;
+    public List<ExampleMenu> getAllMenu() {
+        return repository.getAllMenu();
     }
 
     public MenuService(MenuRepository repository) {
