1a384b3b0c224d24a1fff4107879be55fe0a9a39
means <amajakai14@gmail.com>
2023-04-20 08:33:12 +0900

    migration fix

--- beginner/demo/demo/src/main/resources/local/testdata/afterMigrate.sql
+++ beginner/demo/demo/src/main/resources/local/testdata/afterMigrate.sql
@@ -5,11 +5,11 @@ DELETE FROM example_menu;
 
 INSERT INTO example_user (user_id, password) VALUES ('user1', 'password1');
 INSERT INTO example_stock (item_id, amount) VALUES (1, 3);
-INSERT INTO example_order (item_id, name, amount, order_status, order_date)
+INSERT INTO example_order (id, item_id, name, amount, order_status, order_date)
 VALUES
-(1, 'PO-001', 3, 'PENDING', '2019-01-01')
-,(2, 'PO-002', 2, 'PENDING', '2022-01-01')
-,(3, 'PO-003', 3, 'PENDING', '2022-01-01')
+(1, 1, 'PO-001', 3, 'PENDING', '2019-01-01')
+,(2, 2, 'PO-002', 2, 'PENDING', '2022-01-01')
+,(3, 3, 'PO-003', 3, 'PENDING', '2022-01-01')
 ;
 INSERT INTO example_menu (id, name) VALUES
 (1,'carrot')
